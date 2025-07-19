8ff0ddca59862890b9625155642e1162b3209bf0
premtodev08710 <64456171+premtodev08710@users.noreply.github.com>
2025-07-19 23:42:28 +0700

    ระบบส่งแผนการเรียน

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+// ไฟล์นี้เก็บข้อมูลการตั้งค่า Firebase สำหรับโปรเจกต์ของคุณ
+// เพื่อความปลอดภัย ควรตั้งค่า Rules ใน Firebase Console เพื่อจำกัดการเข้าถึง
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+};
